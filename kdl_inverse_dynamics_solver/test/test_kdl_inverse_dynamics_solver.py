7c80145ff222d08cb452875deb05da10a3ec0d0f
Vincenzo Petrone <vipetrone@unisa.it>
2025-10-09 15:10:11 +0200

    [FIX] Handle empty root and tip parameters
    - [ADD] With empty root, defaults to chain root
    - [ADD] With empty tip, raises an exception
    - [TST] Add two test fixtures to test the two scenarios above

--- kdl_inverse_dynamics_solver/test/test_kdl_inverse_dynamics_solver.py
+++ kdl_inverse_dynamics_solver/test/test_kdl_inverse_dynamics_solver.py
@@ -33,12 +33,19 @@ def generate_test_description():
     )
 
     # Input arguments
+    DEFAULT_GRAVITY = [0, 0, -9.81]
     parameters = {
         "robot_description": robot_description,
         "inverse_dynamics_solver_plugin_name": "kdl_inverse_dynamics_solver/InverseDynamicsSolverKDL",
         "kdl.root": "base_link",
         "kdl.tip": "tool0",
-        "kdl.gravity": [0, 0, -9.81],
+        "kdl.gravity": DEFAULT_GRAVITY,
+        "empty_root.root": "",
+        "empty_root.tip": "tool0",
+        "empty_root.gravity": DEFAULT_GRAVITY,
+        "empty_tip.root": "",  # This setup shall raise a ParameterUninitializedException
+        "empty_tip.tip": "",
+        "empty_tip.gravity": DEFAULT_GRAVITY,
     }
 
     # The node to test
