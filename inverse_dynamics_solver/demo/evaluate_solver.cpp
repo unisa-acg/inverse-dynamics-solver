6c2928dc3f96602c5c465028cc37a070ccbc695e
Vincenzo Petrone <vipetrone@unisa.it>
2026-01-09 22:43:13 +0100

    [FIX] Correct header after backporting

--- inverse_dynamics_solver/demo/evaluate_solver.cpp
+++ inverse_dynamics_solver/demo/evaluate_solver.cpp
@@ -26,7 +26,7 @@
 #include <sensor_msgs/msg/joint_state.hpp>
 #include <pluginlib/class_loader.hpp>
 #include <eigen3/Eigen/Core>
-#include <urdf/model.hpp>
+#include <urdf/model.h>
 #include "inverse_dynamics_solver/inverse_dynamics_solver.hpp"
 
 class EvaluateSolverNode : public rclcpp::Node
